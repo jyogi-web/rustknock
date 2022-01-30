e65c13d4f9cea0d68e19aca2f25be3bc6a6658f0
yukinissie <bigtree2490@outlook.jp>
2022-01-30 19:22:50 +0900

    Docker環境の追加

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello atrix!")
+    HttpResponse::Ok().body("Hello actix!")
 }
 
 #[post("/echo")]
