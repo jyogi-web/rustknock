3650307376cf87c29db02fab6c6f2b18fc30597d
yukinissie <bigtree2490@outlook.jp>
2022-02-02 17:58:13 +0900

    WebSocketのサンプルをモジュール化

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,93 +1,8 @@
-mod quiz;
-// mod ws;
-
-use actix::prelude::*;
 use actix_cors::Cors;
-use actix_web::{get, middleware::Logger, web, App, Error, HttpRequest, HttpResponse, HttpServer};
-use actix_web_actors::ws;
-use std::time::{Duration, Instant};
+use actix_web::{middleware::Logger, App, HttpServer};
 
-/// How often heartbeat pings are sent
-const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
-/// How long before lack of client response causes a timeout
-const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
-
-/// do websocket handshake and start `MyWebSocket` actor
-#[get("/ws/")]
-async fn ws_index(r: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    println!("{:?}", r);
-    let res = ws::start(MyWebSocket::new(), &r, stream);
-    println!("{:?}", res);
-    res
-}
-
-/// websocket connection is long running connection, it easier
-/// to handle with an actor
-struct MyWebSocket {
-    /// Client must send ping at least once per 10 seconds (CLIENT_TIMEOUT),
-    /// otherwise we drop connection.
-    hb: Instant,
-}
-
-impl Actor for MyWebSocket {
-    type Context = ws::WebsocketContext<Self>;
-
-    /// Method is called on actor start. We start the heartbeat process here.
-    fn started(&mut self, ctx: &mut Self::Context) {
-        self.hb(ctx);
-    }
-}
-
-/// Handler for `ws::Message`
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWebSocket {
-    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
-        // process websocket messages
-        println!("WS: {:?}", msg);
-        match msg {
-            Ok(ws::Message::Ping(msg)) => {
-                self.hb = Instant::now();
-                ctx.pong(&msg);
-            }
-            Ok(ws::Message::Pong(_)) => {
-                self.hb = Instant::now();
-            }
-            Ok(ws::Message::Text(text)) => ctx.text(text),
-            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
-            Ok(ws::Message::Close(reason)) => {
-                ctx.close(reason);
-                ctx.stop();
-            }
-            _ => ctx.stop(),
-        }
-    }
-}
-
-impl MyWebSocket {
-    fn new() -> Self {
-        Self { hb: Instant::now() }
-    }
-
-    /// helper method that sends ping to client every second.
-    ///
-    /// also this method checks heartbeats from client
-    fn hb(&self, ctx: &mut <Self as Actor>::Context) {
-        ctx.run_interval(HEARTBEAT_INTERVAL, |act, ctx| {
-            // check client heartbeats
-            if Instant::now().duration_since(act.hb) > CLIENT_TIMEOUT {
-                // heartbeat timed out
-                println!("Websocket Client heartbeat failed, disconnecting!");
-
-                // stop actor
-                ctx.stop();
-
-                // don't try to send a ping
-                return;
-            }
-
-            ctx.ping(b"");
-        });
-    }
-}
+mod quiz;
+mod sample_ws;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -102,7 +17,7 @@ async fn main() -> std::io::Result<()> {
             )
             .wrap(Logger::default())
             .service(quiz::quiz)
-            .service(ws_index)
+            .service(sample_ws::ws_index)
     })
     .bind(("0.0.0.0", 3000))?
     .run()
--- backend/src/quiz/mod.rs
+++ backend/src/quiz/mod.rs
@@ -1,9 +1,8 @@
-
-use actix_web::{App, get, HttpResponse, HttpServer, middleware::Logger, Responder, web};
+use actix_web::{get, HttpResponse, Responder};
 
 mod quiz_json_reader;
 
-#[get("/mod")]
+#[get("/quiz")]
 pub async fn quiz() -> impl Responder {
     HttpResponse::Ok().json(quiz_json_reader::read_file())
 }
