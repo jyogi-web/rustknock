f24622930a9102ea88f3e095adca161fc321a21a
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-02-03 19:08:25 +0900

    loggerおよび便利クレート追加

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,17 @@
+use std::{
+    fs::{self, File},
+    path::Path,
+};
+
 use actix_cors::Cors;
+use actix_files::Files;
 use actix_web::{middleware::Logger, web, App, HttpRequest, HttpServer};
 use build_timestamp::build_time;
+use chrono::{FixedOffset, Utc};
+use log::LevelFilter;
+use simplelog::{
+    ColorChoice, CombinedLogger, ConfigBuilder, SharedLogger, TermLogger, TerminalMode, WriteLogger,
+};
 
 mod quiz;
 mod sample_ws;
@@ -13,21 +24,53 @@ async fn index(req: HttpRequest) -> &'static str {
     BUILD_TIME
 }
 
+fn init_logger(log_path: Option<&str>) {
+    let jst_now = {
+        let jst = Utc::now();
+        jst.with_timezone(&FixedOffset::east(9 * 3600))
+    };
+    let mut logger: Vec<Box<dyn SharedLogger>> = vec![
+        #[cfg(not(feature = "termcolor"))]
+        TermLogger::new(
+            LevelFilter::Debug,
+            ConfigBuilder::new().set_time_to_local(true).build(),
+            TerminalMode::Mixed,
+            ColorChoice::Auto,
+        ),
+    ];
+    if let Some(log_path) = log_path {
+        let log_path = Path::new(log_path);
+        fs::create_dir_all(&log_path).unwrap();
+        logger.push(WriteLogger::new(
+            LevelFilter::Warn,
+            ConfigBuilder::new().set_time_to_local(true).build(),
+            File::create(log_path.join(format!("{}.log", jst_now))).unwrap(),
+        ));
+    }
+    CombinedLogger::init(logger).unwrap()
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // init_logger(Some(&Path::new("./log")));
+    init_logger(None);
+
     std::env::set_var("RUST_LOG", "actix_web=info");
+
     HttpServer::new(|| {
         App::new()
-            .wrap(
+            .wrap(if cfg!(debug_assertions) {
+                Cors::permissive()
+            } else {
                 Cors::default()
                     .allowed_origin("All")
                     .send_wildcard()
-                    .max_age(3600),
-            )
+                    .max_age(3600)
+            })
             .wrap(Logger::default())
             .service(web::resource("/").to(index))
             .service(quiz::quiz)
-            .service(sample_ws::ws_index)
+            .service(Files::new("/static", "./backend/static/").index_file("index.html"))
     })
     .bind(("0.0.0.0", 3000))?
     .run()
