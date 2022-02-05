8e58767f2d738896f538166a3c8bf2e8970c09f5
yukinissie <bigtree2490@outlook.jp>
2022-02-06 00:14:25 +0900

    ルーム名とユーザー名を送れるようになった

--- frontend/src/welcome.rs
+++ frontend/src/welcome.rs
@@ -1,6 +1,8 @@
-use yew::html;
+use wasm_bindgen::JsCast;
+
 use yew::prelude::*;
 
+use yew::{events::Event, html, Component, Context, Html};
 use {
     futures::prelude::*,
     log::*,
@@ -10,10 +12,11 @@ use {
 };
 
 use wasm_bindgen_futures::spawn_local;
+use web_sys::{EventTarget, HtmlInputElement, InputEventInit};
 
 const URL: &str = "ws://127.0.0.1:3000/ws/";
 
-async fn join_room(roomname: String) {
+async fn join_room(roomname: String, username: String) {
     // let _ = console_log::init_with_level(Level::Trace);
 
     // info!はwasm-loggerのマクロだよ！
@@ -28,7 +31,9 @@ async fn join_room(roomname: String) {
     // let mut evts = ws.observe(ObserveConfig::default()).await.expect("observe");
 
     wsio.send(WsMessage::Text(format!("/join {}", roomname)));
+    wsio.send(WsMessage::Text(format!("/name {}", username)));
     info!("/join {:?}", roomname);
+    info!("/name {:?}", username);
 
     // ws.close().await.expect_throw("close ws");
     // info!("Update: {:?}", ws.ready_state());
@@ -40,11 +45,37 @@ async fn join_room(roomname: String) {
 
 #[function_component(Welcome)]
 pub fn welcome() -> Html {
-    let roomname = use_state(|| "oooooooo".to_string());
+    let roomname = use_state(|| "".to_string());
+    let username = use_state(|| "".to_string());
+    let tmp_roomname = roomname.clone();
+    let tmp_username = username.clone();
+
     let onclick = {
         Callback::from(move |_| {
             let roomname = roomname.clone();
-            spawn_local(async move { join_room(roomname.to_string()).await })
+            let username = username.clone();
+            spawn_local(async move { join_room(roomname.to_string(), username.to_string()).await })
+        })
+    };
+
+    let on_roomname_change: Callback<Event> = {
+        Callback::from(move |e: Event| {
+            let target: EventTarget = e
+                .target()
+                .expect("Event should have a target when dispatched");
+
+            let roomname = tmp_roomname.clone();
+            roomname.set(target.unchecked_into::<HtmlInputElement>().value());
+        })
+    };
+    let on_username_change: Callback<Event> = {
+        Callback::from(move |e: Event| {
+            let target: EventTarget = e
+                .target()
+                .expect("Event should have a target when dispatched");
+
+            let username = tmp_username.clone();
+            username.set(target.unchecked_into::<HtmlInputElement>().value());
         })
     };
 
@@ -60,17 +91,17 @@ pub fn welcome() -> Html {
                     <input type="hidden" name="remember" value="true" />
                     <div class="rounded-md shadow-sm -space-y-px">
                         <div>
-                            <label for="room-name" class="sr-only">{"ルーム名"}</label>
-                            <input id="room-name" name="room-name" type="text" required=true class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="ルーム名" />
+                            <label for="roomname" class="sr-only">{"ルーム名"}</label>
+                            <input id="roomname" name="roomname" type="text" required=true onchange={on_roomname_change} class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="ルーム名" />
                         </div>
                         <div>
-                            <label for="user-name" class="sr-only">{"ユーザー名"}</label>
-                            <input id="user-name" name="user-name" type="text" autocomplete="username" required=false class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="ユーザー名" />
+                            <label for="username" class="sr-only">{"ユーザー名"}</label>
+                            <input id="username" name="username" type="text" autocomplete="username" required=false onchange={on_username_change} class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="ユーザー名" />
                         </div>
                     </div>
 
                     <div>
-                      <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" {onclick} disabled=false>
+                      <button type="submit" {onclick} class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                           {"ルームに入る"}
                       </button>
                     </div>
