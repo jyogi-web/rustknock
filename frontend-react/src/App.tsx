407fb72cba56372b939513af5f908c2381f230bd
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-02-06 03:11:54 +0900

    add 2page

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Welcome from "./Welcome";
+import Quiz from "./Quiz";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+interface Props {}
+
+const App: React.FC<Props> = (props) => {
+  const [isWelcome, setIsWelcome] = useState(true);
+
+  return <div>{isWelcome ? <Welcome /> : <Quiz />}</div>;
+};
 
 export default App;
---
+++ frontend-react/src/Quiz.tsx
@@ -0,0 +1,7 @@
+interface Props {}
+
+const Quiz: React.FC<Props> = (props) => {
+  return <div>quiz</div>;
+};
+
+export default Quiz;
