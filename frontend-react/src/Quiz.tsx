f04e97c2238f44d429c09750b450d7ded3205727
yukinissie <bigtree2490@outlook.jp>
2022-02-06 09:23:21 +0900

    大枠

--- frontend-react/src/Quiz.tsx
+++ frontend-react/src/Quiz.tsx
@@ -1,7 +1,25 @@
 interface Props {}
 
 const Quiz: React.FC<Props> = (props) => {
-  return <div>quiz</div>;
+  return (
+    <div className="quiz">
+      {/* メインコンテンツ */}
+      <div className="main">
+        {/* スコアボード */}
+        {/* モニター */}
+        {/* 進行状況 */}
+      </div>
+      {/* 早押しボタン */}
+      <div className="button">
+        {/* ボタン */}
+      </div>
+      {/* 解答フォーム */}
+      <div className="answer-form">
+        {/* 入力フォーム */}
+        {/* 送信ボタン */}
+      </div>
+    </div>
+  );
 };
 
 export default Quiz;
