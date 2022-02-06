48a1dfb69615bb9896b751f6dfd2ad8b83d044eb
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-02-06 15:59:45 +0900

    add pipeline

--- frontend-react/src/Quiz.tsx
+++ frontend-react/src/Quiz.tsx
@@ -1,7 +1,19 @@
-import PropTypes from 'prop-types';
-import { useEffect, useState } from 'react';
-import ScoreBoardItem from './ScoreBoardItem'
-interface Props {}
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import { User, Users } from "./App";
+import ScoreBoardItem from "./ScoreBoardItem";
+interface Props {
+  sendStart: () => void;
+  userData: Users;
+  isStarted: boolean;
+  currentQuestion: string;
+  currentQuestionAnswer: string;
+  answerResult: boolean;
+  othersAnswer: string;
+  sendAnsReq: () => void;
+  isAnswerRight: boolean;
+  sendAnswer: (ans: string) => void;
+}
 
 interface ScoreData {
   username: string;
@@ -14,20 +26,28 @@ const Quiz: React.FC<Props> = (props) => {
   useEffect(() => {
     const sampleData: ScoreData[] = [
       {
-        "username": "nissie",
-        "score": 20
+        username: "nissie",
+        score: 20,
       },
       {
-        "username": "nissie",
-        "score": 20
+        username: "nissie",
+        score: 20,
       },
       {
-        "username": "nissie",
-        "score": 20
+        username: "nissie",
+        score: 20,
       },
-    ]
+    ];
     setScoreData(sampleData);
   }, []);
+
+  let myAnswer = "";
+  const inputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    myAnswer = e.target.value;
+  };
+  const sendMyAnswer = () => {
+    props.sendAnswer(myAnswer);
+  };
 
   return (
     <div className="quiz">
@@ -35,19 +55,37 @@ const Quiz: React.FC<Props> = (props) => {
       <div className="main grid grid-cols-3 pt-20 h-full">
         {/* スコアボード */}
         <div className="scoreboard grid grid-rows">
-          <div className="scoreboard-title flex justify-center items-center border-4 border-sky-200 p-1 mx-20 rounded-t-lg">スコアボード</div>
+          <div className="scoreboard-title flex justify-center items-center border-4 border-sky-200 p-1 mx-20 rounded-t-lg">
+            スコアボード
+          </div>
           <div className="scoreboard-content">
-            <div  className="scoreboard-list">
-              { scoreData && scoreData.map((item: ScoreData) => <ScoreBoardItem username={item.username} score={item.score} />)}
+            <div className="scoreboard-list">
+              {props.userData.userdata &&
+                props.userData.userdata.map((item: User) => (
+                  <ScoreBoardItem username={item.name} score={item.score} />
+                ))}
             </div>
           </div>
-          <div className="scoreboard-title flex justify-center items-center border-4 border-sky-200 p-1 mx-20 rounded-b-lg">　</div>
+          <div className="scoreboard-title flex justify-center items-center border-4 border-sky-200 p-1 mx-20 rounded-b-lg"></div>
         </div>
         {/* モニター */}
         <div className="monitor border-2 border-black flex justify-center items-center text-center rounded-lg">
           <div className="monitor-start">
-            <div className="monitor-text">メンバーは揃いましたか？</div>
-            <button className="start-button border-2 border-black rounded mt-10 px-10 py-2">開始する</button>
+            <div className="monitor-text">
+              {props.isStarted
+                ? "問題 " + props.currentQuestion
+                : "メンバーは揃いましたか？"}
+            </div>
+            {props.isStarted ? (
+              <div />
+            ) : (
+              <button
+                onClick={props.sendStart}
+                className="start-button border-2 border-black rounded mt-10 px-10 py-2"
+              >
+                開始する
+              </button>
+            )}
           </div>
         </div>
         {/* 進行状況 */}
@@ -55,32 +93,52 @@ const Quiz: React.FC<Props> = (props) => {
           {/* 解答 */}
           <div className="user-answer grid grid-rows-1 flex justify-center items-center text-center border-4 border-sky-200 p-1 mx-20 rounded-t-lg">
             <div className="user-answer-key">解答</div>
-            <div className="user-answer-value">ー</div>
+            <div className="user-answer-value">{props.othersAnswer}</div>
           </div>
           {/* 答え */}
           <div className="currect-answer user-answer grid grid-rows-1 flex justify-center items-center text-center border-4 border-sky-200 p-1 my-1 mx-20">
             <div className="currect-answer-key">答え</div>
-            <div className="currect-answer-value">ー</div>
+            <div className="currect-answer-value">
+              {props.currentQuestionAnswer}
+            </div>
           </div>
           {/* 結果 */}
           <div className="result user-answer grid grid-rows-1 flex justify-center items-center text-center border-4 border-sky-200 p-1 mx-20 rounded-b-lg">
             <div className="result-key">結果</div>
-            <div className="result-value">ー</div>
+            <div className="result-value">{props.answerResult ? "○" : "✕"}</div>
           </div>
         </div>
       </div>
       {/* 早押しボタン */}
       <div className="fast-press flex items-center justify-center">
         {/* ボタン */}
-        <button className="fast-press-button border-2 border-rose-700 py-5 px-10 rounded-lg bg-red-600 text-white font-bold text-lg mt-20">早押しボタン</button>
+        <button
+          onClick={props.sendAnsReq}
+          className="fast-press-button border-2 border-rose-700 py-5 px-10 rounded-lg bg-red-600 text-white font-bold text-lg mt-20"
+        >
+          {props.isAnswerRight ? "解答を送信してください" : "早押しボタン"}
+        </button>
       </div>
       {/* 解答フォーム */}
       <form className="answer-form flex items-center justify-center mt-20">
         {/* 入力フォーム */}
-        <label htmlFor="answer" className="sr-only">解答</label>
-        <input type="text" name="answer" id="answer" className="" />
+        <label htmlFor="answer" className="sr-only">
+          解答
+        </label>
+        <input
+          onChange={inputChange}
+          type="text"
+          name="answer"
+          id="answer"
+          className=""
+        />
         {/* 送信ボタン */}
-        <button className="answer-submit-button rounded-lg border-2 border-sky-700 p-2 bg-sky-500 text-white">送信</button>
+        <button
+          onClick={sendMyAnswer}
+          className="answer-submit-button rounded-lg border-2 border-sky-700 p-2 bg-sky-500 text-white"
+        >
+          送信
+        </button>
       </form>
     </div>
   );
